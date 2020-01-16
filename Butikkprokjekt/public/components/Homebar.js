c35e9d031ba3ad5a52971b6be502f54ae2ca43ab
Simon Bjørheim <simonbjorheim@Simons-MacBook-Air.local>
2020-01-16 08:42:27 +0100

    changes

--- Butikkprokjekt/public/components/Homebar.js
+++ Butikkprokjekt/public/components/Homebar.js
@@ -30,7 +30,7 @@ class HomeBar extends HTMLElement {
                 align-items: center;
                 grid-template-columns: 1fr 1fr 2fr 3fr 1fr;
                 height: 70px;
-                background-color: rgba(32,166,231,.8);
+                background-color: #666;
                 background-image: var(--grad, linear-gradient(180deg,#20a8e9,rgba(30,158,220,.5)) );
                 color: #fff;
             }
@@ -57,6 +57,7 @@ class HomeBar extends HTMLElement {
               border: solid gray 1px;
               border-radius: 4px;
               padding: 5px;
+            
             }
 
             div#menu:focus-within ul,
